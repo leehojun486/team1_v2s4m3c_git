54b8071dd462719da96cabaadef4fddcc0916a54
MACwindows <MACwindows@DESKTOP-1CM9BR4>
2021-01-22 15:12:10 +0900

    faq packege 수정

--- src/main/java/dev/mvc/faq/Contents.java
+++ src/main/java/dev/mvc/faq/Contents.java
@@ -5,7 +5,7 @@ public class Contents {
   public static int RECORD_PER_PAGE = 3;
   
   /** 블럭당 페이지 수, 하나의 블럭은 1개 이상의 페이지로 구성됨 */
-  public static int PAGE_PER_BLOCK = 2;
+  public static int PAGE_PER_BLOCK = 5;
   
 }
 
