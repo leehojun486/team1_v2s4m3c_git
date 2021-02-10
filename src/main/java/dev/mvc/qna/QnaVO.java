0ced46e57c83a4ca3d39d372574c7358811ea6ab
aorao <aorao@DESKTOP-4J4EMAE>
2021-02-10 15:57:57 +0900

    qna테이블 추가함

---
+++ src/main/java/dev/mvc/qna/QnaVO.java
@@ -0,0 +1,16 @@
+package dev.mvc.qna;
+
+/*qnano NUMERIC(10) NOT NULL PRIMARY KEY,
+memberno NUMERIC(10) NOT NULL,
+adminno NUMERIC(6) NULL,
+qnatitle VARCHAR(50) NOT NULL,
+qnacontent VARCHAR(500) NOT NULL,
+qnacondition VARCHAR(50) NOT NULL,
+qnapasswd VARCHAR(15) NOT NULL,
+qnardate DATE NOT NULL,
+FOREIGN KEY (adminno) REFERENCES admin (adminno),
+FOREIGN KEY (memberno) REFERENCES member (memberno)*/
+
+public class QnaVO {
+
+}
